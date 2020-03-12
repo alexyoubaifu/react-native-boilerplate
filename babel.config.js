66275669a1231afadebc4477bc13f76d8dcb13e2
alexyoubaifu <alex.you@baifu-tech.net>
2020-03-12 10:07:00 +0800

    Add packages

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset', 'mobx'],
+  presets: ['module:metro-react-native-babel-preset', '@babel/preset-flow'],
   plugins: [
-    ['@babel/plugin-transform-flow-strip-types'],
+    '@babel/transform-flow-strip-types',
     ['@babel/plugin-proposal-decorators', { legacy: true }],
     ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-transform-runtime', {}],
   ],
 };
